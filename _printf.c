544ff3fb6a34237e1b27e036cf5ec16f46a81ba6
ubonisrael <akpanudohubonisrael@gmail.com>
2023-09-15 18:39:00 +0100

    tasks 0 and 1

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "printf.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format
+ * Return: 0
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int x = 0;
+	va_list args;
+
+	va_start(args, format);
+	/* iterate through the string */
+	while (format[x])
+	{
+		/* % in ASCII = 37 */
+		/* check if the character currently pointed to is % */
+		if (format[x] == 37)
+		{
+			/* pass the next character i.e x+1 */
+			/* to the get format function */
+			(*get_format_func(format[x + 1]))(args);
+			/* increment x to skip this character */
+			x++;
+		}
+		else
+		{
+			/* it's a regular character, print it with _putchar */
+			_putchar(format[x]);
+		}
+		x++;
+	}
+	va_end(args);
+	return (x);
+}
---
+++ get_format_func.c
@@ -0,0 +1,28 @@
+#include "printf.h"
+
+/**
+ * get_format_func - gets the appropriate function for the operation
+ * @num: format
+ * Return: a pointer to the function or NULL if not found
+ */
+
+void (*get_format_func(int num))(va_list args)
+{
+	/* create an array of formats and their corresponding functions */
+	format_list formats[] = {{99, print_char},
+				 {115, print_string},
+				 {100, print_int},
+				 {105, print_unsigned_int},
+				 {0, NULL}};
+	unsigned int x = 0;
+
+	/* iterate through the array, if any of the formats match */
+	/* return the corresponding function */
+	while (formats[x].n != 0)
+	{
+		if (formats[x].n == num)
+			return (formats[x].func);
+		x++;
+	}
+	return (NULL);
+}
